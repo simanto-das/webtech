d79cc533627f5b7913e820d1f483f546c23b442a
Adit-ahnaf <45269462+Adit-ahnaf@users.noreply.github.com>
2020-12-12 00:59:19 +0600

    updated

--- Final/Project/Shop/Controller/ActionAssignProductDetails.php
+++ Final/Project/Shop/Controller/ActionAssignProductDetails.php
@@ -1,24 +1,33 @@
+
 <?php
 include '../Model/dbconnection.php';
-include '../View/AssignProductDetails.php';
 
 $prid = mysqli_real_escape_string($conn, $_REQUEST['id']);
 $prname = mysqli_real_escape_string($conn, $_REQUEST['pn']);
 $prprice = mysqli_real_escape_string($conn, $_REQUEST['pr']);
 $prdescription = mysqli_real_escape_string($conn, $_REQUEST['pd']);
 $prcategory = mysqli_real_escape_string($conn, $_REQUEST['ct']);
-$primage = mysqli_real_escape_string($conn, $_REQUEST['filename']);
+$primage = $_FILES['filename']['name'];
 
-$sql = "INSERT INTO product (id, name, price, description, category, image) VALUES ('$prid',$prname', '$prprice','$prdescription', '$prcategory','$primage')";
+ if(file_exists("../Controller/uploads/".$_FILES["filename"]["name"])){
+ 	echo "Image already exicts";
+ }
+ else{
+$sql = "INSERT INTO product (id, name, price, description, category, image) VALUES ('$prid', '$prname','$prprice', '$prdescription','$prcategory','$primage')";
 if(mysqli_query($conn, $sql)){
     echo "Records added successfully.";
-    header("Location: Table.php");
+   
+    move_uploaded_file($_FILES['filename']['tmp_name'],"../Controller/uploads/".$_FILES['filename']['name']);
+    
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}  
 }
  
 
 mysqli_close($conn);
+
+
  
 
 
